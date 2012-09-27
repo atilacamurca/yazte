32f0c6964a08088be4e4e942ffbd6a575c3d4b89
atilacamurca <camurca.home@gmail.com>
2012-09-27 14:55:30 -0300

    project structure

---
+++ application/views/helpers/FlashMessages.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of FlashMessages
+ *
+ * @author atila
+ */
+class Zend_View_Helper_FlashMessages extends Zend_View_Helper_Abstract {
+
+   public function flashMessages() {
+      $messages = Zend_Controller_Action_HelperBroker::getStaticHelper('FlashMessenger')->getMessages();
+      $output = '';
+
+      if (!empty($messages)) {
+         foreach ($messages as $message) {
+            $output .= '<div class="alert fade in alert-' . key($message) . '">';
+            $output .= '<a class="close" data-dismiss="alert" href="#">×</a>';
+            $output .= '<h4 class="alert-heading">';
+            switch (key($message)) {
+               case 'block':
+                  $output .= 'Be careful!';
+                  break;
+               case 'info':
+                  $output .= 'Heads up!';
+                  break;
+               case 'error':
+                  $output .= 'Oh snap!';
+                  break;
+               case 'success':
+                  $output .= 'Well Done!';
+                  break;
+            }
+            $output .= '</h4>'. current($message) . '</div>';
+         }
+      }
+      return $output;
+   }
+}
+
+?>
