b12ec64bc85e859e323ea6f144de35de6a14fb94
atilacamurca <camurca.home@gmail.com>
2012-09-28 09:27:14 -0300

    able to create and manage a connection

--- library/Yazte/Test.php
+++ library/Yazte/Test.php
@@ -1,5 +1,14 @@
 <?php
 
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+    '../../library',
+    get_include_path(),
+)));
+
+require_once "Zend/Db.php";
+//require_once "Zend/Db/Table.php";
+
 class Yazte_Test {
 	
 	function getTemplate($file, $vars = array()) {
@@ -9,7 +18,28 @@ class Yazte_Test {
 		}
 		return ob_get_clean();
 	}
+   
+   /*function getDefaultAdapter() {
+      $dbAdapter = Zend_Db_Table::getDefaultAdapter();
+      var_dump($dbAdapter);
+   }*/
+   
+   function getAdapter() {
+      $db = Zend_Db::factory('PDO_PGSQL', array(
+                                                'host'     => 'localhost',
+                                                'username' => 'postgres',
+                                                'password' => 'postgres',
+                                                'dbname'   => 'yazte'
+                                            ));
+      //var_dump($db);
+      $db->setFetchMode(Zend_Db::FETCH_OBJ);
+      $row = $db->fetchAll("select name from projects");
+      
+      echo $row[0]->name;
+      echo "\n";
+   }
 }
 
 $test = new Yazte_Test();
-echo $test->getTemplate('file.php', array('var1' => 'atilacamurca'));
+//echo $test->getTemplate('file.php', array('var1' => 'atilacamurca'));
+$test->getAdapter();
